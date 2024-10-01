a7c3f24a0f1209084bbec2336b41d98199f90b4a
ramirachdi <rachdirami11@gmail.com>
2024-10-01 15:59:16 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const port = 3000;
+
+app.get('/api/secret', (req, res) => {
+  const secretKey = process.env.MY_SECRET_KEY;
+  res.json({ secretKey });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
